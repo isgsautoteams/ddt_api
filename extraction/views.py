95655c9b8cf73a45c28be30bc48eceaf5e584d1f
DDT <root@localhost.localdomain>
2024-03-25 15:31:19 +0700

    error handling + settings

--- extraction/views.py
+++ extraction/views.py
@@ -1,19 +1,44 @@
+import mimetypes
+from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, HttpResponse
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .form import UploadFileForm
 from .function import handle_uploaded_file
 from .extraction import extract_text
- 
-# Create your views here.
+
+ALLOWED_MIME_TYPES = [
+    'application/pdf',
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+]
+
+def handle_uploaded_file(file):
+    # Check if the file is a PDF
+    if file.content_type not in ALLOWED_MIME_TYPES:
+        raise ValueError("Please upload file in the form of images or pdf")
+    if file.size > 10000000:
+        raise ValueError("You cannot upload file more than 10MB")
+    # Continue with the file processing
+    filename = f'{file.name}.pdf'
+    with open(filename, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+    
+    return filename
+
 def index(request):
     if request.method == 'POST':
       form = UploadFileForm(request.POST, request.FILES)
       if form.is_valid():
-        filename = handle_uploaded_file(request.FILES['file'])
-        extract_result = extract_text(filename)
-        return HttpResponse(extract_result)
+        try:
+            filename = handle_uploaded_file(request.FILES['file'])
+            extract_result = extract_text(filename)
+            return HttpResponse(extract_result)
+        except ValueError as e:
+            return HttpResponseBadRequest(f'Error while making the API request: {e}')
       else:
-        return HttpResponse(f'{form.errors}')
-         
+          return HttpResponseBadRequest(f'There is an error while making the API request : {form.errors}')
+
     else:
-      return HttpResponse('Hello From OCR')
+      return HttpResponseNotAllowed(['GET'], 'For this API to work, use the POST method')
+
